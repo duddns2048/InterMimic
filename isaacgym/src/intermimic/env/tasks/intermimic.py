9e7a855d98a81fec2c8583ac68892063885adadd
youngwoon <duddns2048@korea.ac.kr>
2026-02-14 01:04:51 +0900

    -minor change: 주석 추가, 정리

--- isaacgym/src/intermimic/env/tasks/intermimic.py
+++ isaacgym/src/intermimic/env/tasks/intermimic.py
@@ -553,27 +553,27 @@ class InterMimic(Humanoid_SMPLX):
             root_h_obs = torch.zeros_like(root_h)
         else:
             root_h_obs = root_h
-
+        # ----------- heading rot for key pos 21-----------
         len_keypos = len(key_body_ids)
         heading_rot_expand = heading_rot.unsqueeze(-2)
         heading_rot_expand_2 = heading_rot_expand.repeat((1, len_keypos, 1))
         flat_heading_rot_2 = heading_rot_expand_2.reshape(heading_rot_expand_2.shape[0] * heading_rot_expand_2.shape[1], 
                                                 heading_rot_expand_2.shape[2])
-        
+        # ----------- heading rot for body pos 52-----------
         heading_rot_expand = heading_rot_expand.repeat((1, body_pos.shape[1], 1))
         flat_heading_rot = heading_rot_expand.reshape(heading_rot_expand.shape[0] * heading_rot_expand.shape[1], 
                                                 heading_rot_expand.shape[2])
-
+        # ----------- heading rot for body pos - 2 hands -----------
         heading_rot_expand = heading_rot.unsqueeze(-2)
         heading_rot_expand_no_hand = heading_rot_expand.repeat((1, 22, 1))
         flat_heading_rot_no_hand = heading_rot_expand_no_hand.reshape(heading_rot_expand_no_hand.shape[0] * heading_rot_expand_no_hand.shape[1], 
                                                 heading_rot_expand_no_hand.shape[2])
-
+        # ----------- inv heading rot for body pos -----------
         heading_inv_rot_expand = heading_inv_rot.unsqueeze(-2)
         heading_inv_rot_expand = heading_inv_rot_expand.repeat((1, body_pos.shape[1], 1))
         flat_heading_inv_rot = heading_inv_rot_expand.reshape(heading_inv_rot_expand.shape[0] * heading_inv_rot_expand.shape[1], 
                                                 heading_inv_rot_expand.shape[2])
-
+        # ----------- inv heading rot for body pos - 2 hands -----------
         heading_inv_rot_expand = heading_inv_rot.unsqueeze(-2)
         heading_inv_rot_expand_no_hand = heading_inv_rot_expand.repeat((1, 22, 1))
         flat_heading_inv_rot_no_hand = heading_inv_rot_expand_no_hand.reshape(heading_inv_rot_expand_no_hand.shape[0] * heading_inv_rot_expand_no_hand.shape[1], 
@@ -587,19 +587,22 @@ class InterMimic(Humanoid_SMPLX):
         
         local_ref_body_pos = _body_pos - root_pos.unsqueeze(1)  # preserves the body position
         local_ref_body_pos = torch_utils.quat_rotate(flat_heading_rot_2, local_ref_body_pos.view(-1, 3)).view(-1, len_keypos * 3)
-    
+        
+        # ============= local_body_pos_obs =================
         root_pos_expand = root_pos.unsqueeze(-2)
         local_body_pos = body_pos - root_pos_expand
         flat_local_body_pos = local_body_pos.reshape(local_body_pos.shape[0] * local_body_pos.shape[1], local_body_pos.shape[2])
         flat_local_body_pos = quat_rotate(flat_heading_rot, flat_local_body_pos)
-        local_body_pos = flat_local_body_pos.reshape(local_body_pos.shape[0], local_body_pos.shape[1] * local_body_pos.shape[2])
-        local_body_pos = local_body_pos[..., 3:] # remove root pos
+        local_body_pos_obs = flat_local_body_pos.reshape(local_body_pos.shape[0], local_body_pos.shape[1] * local_body_pos.shape[2])
+        local_body_pos_obs = local_body_pos_obs[..., 3:] # remove root pos
 
-        flat_body_rot = body_rot.reshape(body_rot.shape[0] * body_rot.shape[1], body_rot.shape[2])
-        flat_local_body_rot = quat_mul(flat_heading_rot, flat_body_rot)
-        flat_local_body_rot_obs = torch_utils.quat_to_tan_norm(flat_local_body_rot)
-        local_body_rot_obs = flat_local_body_rot_obs.reshape(body_rot.shape[0], body_rot.shape[1] * flat_local_body_rot_obs.shape[1])
-        
+        # ============= local_body_rot_obs =================
+        flat_body_rot = body_rot.reshape(body_rot.shape[0] * body_rot.shape[1], body_rot.shape[2]) # (2048 * 52, 4)
+        flat_local_body_rot = quat_mul(flat_heading_rot, flat_body_rot) # (2048 * 52, 4)
+        flat_local_body_rot_obs = torch_utils.quat_to_tan_norm(flat_local_body_rot) # (2048 * 52, 6)
+        local_body_rot_obs = flat_local_body_rot_obs.reshape(body_rot.shape[0], body_rot.shape[1] * flat_local_body_rot_obs.shape[1]) # (2048, 312)
+
+        # ============= diff_local_body_rot_obs =================
         ref_body_rot = self.extract_data_component('body_rot', obs=ref_obs)
         ref_body_rot_no_hand = torch.cat((ref_body_rot[:, :18*4], ref_body_rot[:, 33*4:37*4]), dim=-1) 
         body_rot_no_hand = torch.cat((body_rot[:, :18], body_rot[:, 33:37]), dim=1)
@@ -607,10 +610,11 @@ class InterMimic(Humanoid_SMPLX):
         diff_local_body_rot_flat = torch_utils.quat_mul(torch_utils.quat_mul(flat_heading_rot_no_hand, diff_global_body_rot.view(-1, 4)), flat_heading_inv_rot_no_hand)
         diff_local_body_rot_obs = torch_utils.quat_to_tan_norm(diff_local_body_rot_flat)
         diff_local_body_rot_obs = diff_local_body_rot_obs.view(body_rot_no_hand.shape[0], body_rot_no_hand.shape[1] * diff_local_body_rot_obs.shape[-1])
-
+        
         local_ref_body_rot = torch_utils.quat_mul(flat_heading_rot_no_hand, ref_body_rot_no_hand.reshape(-1, 4))
         local_ref_body_rot = torch_utils.quat_to_tan_norm(local_ref_body_rot).view(ref_body_rot_no_hand.shape[0], -1)
 
+        # ============= local key_body vel diff (2048, 63)=================
         ref_body_vel = self.extract_data_component('body_pos_vel', obs=ref_obs).view(ref_obs.shape[0], -1, 3)[:, key_body_ids, :]
         _body_vel = body_vel[:, key_body_ids, :]
         diff_global_vel = ref_body_vel - _body_vel
@@ -622,24 +626,29 @@ class InterMimic(Humanoid_SMPLX):
         diff_global_ang_vel = ref_body_ang_vel_no_hand.view(-1, 22, 3) - body_ang_vel_no_hand
         diff_local_ang_vel = torch_utils.quat_rotate(flat_heading_rot_no_hand, diff_global_ang_vel.view(-1, 3)).view(-1, 22 * 3)
 
+        # ============= local_body_rot_obs =================
         if (local_root_obs):
             root_rot_obs = torch_utils.quat_to_tan_norm(root_rot)
             local_body_rot_obs[..., 0:6] = root_rot_obs
 
+        # ============= local_body_vel =================
         flat_body_vel = body_vel.reshape(body_vel.shape[0] * body_vel.shape[1], body_vel.shape[2])
         flat_local_body_vel = quat_rotate(flat_heading_rot, flat_body_vel)
         local_body_vel = flat_local_body_vel.reshape(body_vel.shape[0], body_vel.shape[1] * body_vel.shape[2])
         
+        # ============= local_body_ang_vel =================
         flat_body_ang_vel = body_ang_vel.reshape(body_ang_vel.shape[0] * body_ang_vel.shape[1], body_ang_vel.shape[2])
         flat_local_body_ang_vel = quat_rotate(flat_heading_rot, flat_body_ang_vel)
         local_body_ang_vel = flat_local_body_ang_vel.reshape(body_ang_vel.shape[0], body_ang_vel.shape[1] * body_ang_vel.shape[2])
-
+        
+        # ============= contact (21 body + 10 finger tips) =================
         body_contact_buf = contact_forces[:, contact_body_ids, :].clone() #.view(contact_forces.shape[0],-1)
-        contact = torch.any(torch.abs(body_contact_buf) > 0.1, dim=-1).float()
+        contact = torch.any(torch.abs(body_contact_buf) > 0.1, dim=-1).float() # (num_envs, 31)
+
         ref_body_contact = self.extract_data_component('contact_human', obs=ref_obs)[:, contact_body_ids]
         diff_body_contact = ref_body_contact * ((ref_body_contact + 1) / 2 - contact)
 
-        obs = torch.cat((root_h_obs, local_body_pos, local_body_rot_obs, local_body_vel, local_body_ang_vel, contact, diff_local_body_pos_flat, diff_local_body_rot_obs, diff_body_contact, local_ref_body_pos, local_ref_body_rot, diff_local_vel, diff_local_ang_vel), dim=-1)
+        obs = torch.cat((root_h_obs, local_body_pos_obs, local_body_rot_obs, local_body_vel, local_body_ang_vel, contact, diff_local_body_pos_flat, diff_local_body_rot_obs, diff_body_contact, local_ref_body_pos, local_ref_body_rot, diff_local_vel, diff_local_ang_vel), dim=-1)
         return obs
     
     def compute_obj_observations(self, root_states, tar_states, ref_obs):
@@ -662,7 +671,7 @@ class InterMimic(Humanoid_SMPLX):
 
         local_tar_rot = quat_mul(heading_rot, tar_rot)
         local_tar_rot_obs = torch_utils.quat_to_tan_norm(local_tar_rot)
-
+        # ----------------------------------------------------
         _ref_obj_pos = self.extract_data_component('obj_pos', obs=ref_obs)
         diff_global_obj_pos = _ref_obj_pos - tar_pos
         diff_local_obj_pos_flat = torch_utils.quat_rotate(heading_rot, diff_global_obj_pos)
@@ -696,9 +705,9 @@ class InterMimic(Humanoid_SMPLX):
         ts = self.progress_buf[env_ids].clone() 
         next_ts = torch.clamp(ts + delta_t, max=self.max_episode_length[self.data_id[env_ids]]-1)
         ref_obs = hoi_data[self.data_id[env_ids], next_ts].clone()
-        obs = self._compute_humanoid_obs(env_ids, ref_obs, next_ts)
-        task_obs = self._compute_task_obs(env_ids, ref_obs)
-        obs = torch.cat([obs, task_obs], dim=-1)    
+        obs = self._compute_humanoid_obs(env_ids, ref_obs, next_ts) # [2048, 1359]
+        task_obs = self._compute_task_obs(env_ids, ref_obs) # [2048, 21]
+        obs = torch.cat([obs, task_obs], dim=-1)     # [2048, 1380]
         ig_all, ig, ref_ig = self._compute_ig_obs(env_ids, ref_obs)
         return torch.cat((obs,ig_all,ref_ig-ig),dim=-1)
         
@@ -762,7 +771,10 @@ class InterMimic(Humanoid_SMPLX):
         ig = quat_rotate(heading_rot_extend, ig).view(tar_pos.shape[0], -1)    
         
         obs = torch.cat((root_pos, root_rot, dof_pos, dof_vel, 
-                         body_pos.reshape(body_pos.shape[0],-1), body_rot.reshape(body_rot.shape[0],-1), body_vel.reshape(body_vel.shape[0],-1), body_rot_vel.reshape(body_rot_vel.shape[0],-1),
+                         body_pos.reshape(body_pos.shape[0],-1), 
+                         body_rot.reshape(body_rot.shape[0],-1), 
+                         body_vel.reshape(body_vel.shape[0],-1), 
+                         body_rot_vel.reshape(body_rot_vel.shape[0],-1),
                          target_states, ig, contact, target_contact), dim=-1)
         return obs
 
