f3ee2de507e2fe3f4b029d99ff6cee4bdc9d4ced
youngwoon <duddns2048@korea.ac.kr>
2026-03-05 02:04:15 +0900

    train parahome sh

--- isaacgym/scripts/train_parahome.sh
+++ isaacgym/scripts/train_parahome.sh
@@ -7,14 +7,15 @@ REPO_ROOT="$(CDPATH= cd -- "${SCRIPT_DIR}/../.." && pwd)"
 export PYTHONPATH="$REPO_ROOT/isaacgym/src:$REPO_ROOT:$PYTHONPATH"
 
 python -m intermimic.run \
-    --task InterMimic \
+    --task InterMimic_Parahome \
     --cfg_env isaacgym/src/intermimic/data/cfg/parahome_train.yaml \
     --cfg_train isaacgym/src/intermimic/data/cfg/train/rlg/parahome.yaml \
     --output checkpoints \
-    --experiment "[Intermimic]_sub1_clothesstand" \
     --num_envs 2048 \
     --minibatch_size 16384\
-    --motion_file InterAct/single/sub1_clothesstand \
+    --experiment "[Intermimic_para]_sub1_clothesstand" \
+    --motion_file InterAct/Parahome/s110_0_kettle_table2desk \
+    --robotType parahome/sim_human/s110_intermimic.xml \
     --wandb_project intermimic \
     --headless \
     # --resume_from checkpoints/s110/nn/s110_kettle_overfitting_tmp_tmp_latest.pth \
